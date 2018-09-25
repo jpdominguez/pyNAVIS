5bc077a1f7976fc8206599fe6264e69589910c84
Juan Pedro Dominguez-Morales <jpdominguez@atc.us.es>
2018-09-25 15:54:47 +0200

    Clean main

--- main.py
+++ main.py
@@ -6,39 +6,25 @@ from pyNAVIS_settings.main_settings import *
 
 
 #path = 'D:\\Repositorios\\GitHub\\pyNAVIS\\1khz_test_pdm_mono_30att2.aedat'
-path = 'D:\\Repositorios\\GitHub\\pyNAVIS\\c120e80e_nohash_8.wav.aedat'
-#path = 'D:\\Repositorios\\GitHub\\pyNAVIS\\train_cut.aedat'
+#path = 'D:\\Repositorios\\GitHub\\pyNAVIS\\c120e80e_nohash_8.wav.aedat'
+path = 'D:\\Repositorios\\GitHub\\pyNAVIS\\train_cut.aedat'
 
-#settings = MainSettings(p_num_channels=64, p_mono_stereo=1, p_address_size=4, p_ts_tick=0.1, p_bin_size=2000, p_bar_line=1, p_spikegram_dot_freq=100)
-settings = MainSettings(p_num_channels=32, p_mono_stereo=0, p_address_size=2, p_ts_tick=0.2, p_bin_size=20000, p_bar_line=1, p_spikegram_dot_freq=2)
+settings = MainSettings(p_num_channels=64, p_mono_stereo=1, p_address_size=4, p_ts_tick=0.1, p_bin_size=2000, p_bar_line=1, p_spikegram_dot_freq=100)
+#settings = MainSettings(p_num_channels=32, p_mono_stereo=0, p_address_size=2, p_ts_tick=0.2, p_bin_size=20000, p_bar_line=1, p_spikegram_dot_freq=2)
 
 add, ts = loadAERDATA(path, settings)
 ts = adaptAERDATA(ts, settings)
 checkAERDATA(add, ts, settings)
-0.
+
 print "The file contains", len(add), "spikes"
 print "The audio has", max(ts), 'microsec'
 
 #print execution_time(loadAERDATA, [path])
 
 print execution_time(spikegram, (add, ts, settings))
-
-
 print execution_time(sonogram, (add, ts, settings))
-
 print execution_time(histogram, (add, settings))
-
 print execution_time(average_activity,(add, ts, settings))
-
-"""
 print execution_time(difference_between_LR, (add, ts, settings))
-"""
-
-"""
-wav_data, wav_fs = wav_loadWAV('D:\\Repositorios\\GitHub\\pyNAVIS\\c120e80e_nohash_8.wav')
-wav_plot(wav_data, wav_fs)
-wav_spectrogram(wav_data, wav_fs)
-"""
-
 
 raw_input()
