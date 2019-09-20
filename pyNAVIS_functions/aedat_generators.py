5ff8eeb1009e155de76d8c6fef6e4439da02bf48
Juan P. Dominguez-Morales <jpdominguez@atc.us.es>
2019-09-20 02:50:55 +0200

    Fixed Sweep functionaliy
    Working!

--- pyNAVIS_functions/aedat_generators.py
+++ pyNAVIS_functions/aedat_generators.py
@@ -4,18 +4,28 @@ from pyNAVIS_functions.aedat_functions import save_AERDATA
 from pyNAVIS_settings.main_settings import MainSettings
 
 def sweep(freq, cycles, num_ch, length, path):
-    addrs = [0] * int(freq*length)
-    timestamps = [0] * int(freq*length)
+    addrs = [0] * int(freq*cycles*(num_ch*2-1))
+    timestamps = [0] * int(freq*cycles*(num_ch*2-1))
+    current_spike_id = 0
+
+    time_per_cycle = float(length) / cycles
+    spikes_per_cycle = freq * (num_ch * 2 - 1)
+    time_between_spikes = float(time_per_cycle) / spikes_per_cycle
 
     for c in range(cycles):
-        for i in range((num_ch-1)*2):
-            for j in range((freq*length/num_ch)/cycles - 1):
+        for i in range((num_ch*2)-1):
+            #for j in range((freq*length/num_ch)/cycles - 1):
+            for j in range(freq):
                 #try:
-                current_spike_id = j + i*((freq*length/num_ch)/cycles - 1) + c*((num_ch-1)*2)*((freq*length/num_ch)/cycles - 1) #check
-                print current_spike_id
-                print c, i, j
-                addrs[current_spike_id] = (1 - i/num_ch)*i + (i/num_ch)*(num_ch - (i+1)%num_ch)
-                timestamps[current_spike_id] = int(current_spike_id*1000000/freq) #Multiplying by 1000000 to convert timestamps to microseconds  
+                #current_spike_id = j + i*((freq*length/num_ch)/cycles - 1) + c*((num_ch-1)*2)*((freq*length/num_ch)/cycles - 1) #check
+                #addrs[current_spike_id] = (1 - i/num_ch)*i + (i/num_ch)*(num_ch - (i+1)%num_ch)
+                addrs[current_spike_id] = i - (i/num_ch)*(((i%(num_ch))+1)*2)
+                #print i, (i/num_ch)*(((i%(num_ch))+1)*2), i - (i/num_ch)*(((i%(num_ch))+1)*2)
+
+                #timestamps[current_spike_id] = int(current_spike_id*1000000/freq) #Multiplying by 1000000 to convert timestamps to microseconds  
+                timestamps[current_spike_id] = int(current_spike_id*time_between_spikes)
+
+                current_spike_id += 1
                 #except:
                 #    print 'Error', current_spike_id
     print addrs
