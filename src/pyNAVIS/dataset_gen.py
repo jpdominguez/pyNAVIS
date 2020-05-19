df8366100644524abef6e8e17adfd97d05d83062
Juan P. Dominguez-Morales <jpdominguez@atc.us.es>
2020-05-19 10:21:05 +0200

    Added phaselocked dataset generation

--- src/pyNAVIS/dataset_gen.py
+++ src/pyNAVIS/dataset_gen.py
@@ -26,6 +26,7 @@ import matplotlib
 from .functions import Functions
 from .loaders import Loaders
 from .plots import Plots
+from .savers import Savers
 
 class DatasetGenerators:
 
@@ -46,7 +47,7 @@ class DatasetGenerators:
         """
 
         if verbose == True:
-            print("---- DATASET SONOGRAM GENERATION ----")
+            print("---- SONOGRAM DATASET GENERATION ----")
             start_time = time.time()    
         if not os.path.exists(path_output_folder):
             os.makedirs(path_output_folder)
@@ -63,6 +64,49 @@ class DatasetGenerators:
             spikes_file = Loaders.loadAERDATA(file, settings)
             sonogram_data = Plots.sonogram(spikes_file.addresses, spikes_file.timestamps, settings, return_data = True)
             matplotlib.image.imsave(os.path.join(path_output_folder, os.path.basename(file)) + '.png', sonogram_data)
+            
+            if verbose == True:
+                progress += 1
+                print("Progress: " + str(float(progress)*100/len(files)) + "%")
+        
+        if verbose == True: print('Dataset generation process completed. Took ' + str(time.time()-start_time) + " seconds.")
+
+
+    def generate_phaselock_dataset(path_input_folder, path_output_folder, settings, allow_subdirectories = False, verbose = False):
+        """
+        Automatically generates and saves phaselocked AER-DATA files from a set of AER-DATA files.
+        
+        Parameters:
+                path_input_folder (string): Path of the folder where AER-DATA files are.
+                path_output_folder (string): Path of the folder where phaselocked AER-DATA files will be saved.
+                settings (MainSettings): Configuration parameters of the files in the path_input_folder.
+                allow_subdirectories (boolean, optional): Allow the function to navigate deeper in the path_input_folder folder structure to look for input files.
+                verbose (boolean, optional): Set to True if you want the execution time of the function to be printed.
+
+        Returns:
+                None.
+        """
+
+        if verbose == True:
+            print("---- PHASELOCK DATASET GENERATION ----")
+            start_time = time.time()    
+        if not os.path.exists(path_output_folder):
+            os.makedirs(path_output_folder)
+        files = []
+        progress = 0    
+        for r, d, f in os.walk(path_input_folder): # r=root, d=directories, f = files
+            for file in f:
+                if '.aedat' in file:
+                    files.append(os.path.join(r, file))
+            if allow_subdirectories == False:
+                break
+
+        for file in files:
+            spikes_file = Loaders.loadAERDATA(file, settings)
+            #sonogram_data = Plots.sonogram(spikes_file.addresses, spikes_file.timestamps, settings, return_data = True)
+            #matplotlib.image.imsave(os.path.join(path_output_folder, os.path.basename(file)) + '.png', sonogram_data)
+            phaselocked_spikes = Functions.phase_lock(spikes_file, settings)
+            Savers.save_AERDATA(phaselocked_spikes, os.path.join(path_output_folder, os.path.basename(file)), settings)
             
             if verbose == True:
                 progress += 1
