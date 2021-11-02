e5719ccc12ed400eb9076f124ff51ea89571ffb1
alvaroy96 <alv.correo@gmail.com>
2021-11-02 17:40:59 +0100

    PDF_report function has been optimized

--- src/pyNAVIS/plots.py
+++ src/pyNAVIS/plots.py
@@ -21,24 +21,21 @@
 
 import math
 import random
-from bisect import bisect_left, bisect_right
-from matplotlib.colors import LinearSegmentedColormap
-
-import matplotlib
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
 import time
+from bisect import bisect_left, bisect_right
 
-from .utils import Utils
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
 
 
 class Plots:
     @staticmethod
-    def spikegram(spikes_file, settings, dot_size = 0.2, dot_freq = 1, graph_title = 'Spikegram', start_at_zero = True, verbose = False):
+    def spikegram(spikes_file, settings, dot_size=0.2, dot_freq=1, graph_title='Spikegram', start_at_zero=True,
+                  verbose=False):
         """
         Plots the spikegram (also known as cochleogram or raster plot) of a SpikesFile.
-        
+
         This is, a graph where the X axis means time and the Y axis represents addresses (or cochlea channels), and where every spike is plotted as a dot.
 
         Parameters:
@@ -53,12 +50,12 @@ class Plots:
         Returns:
                 None.
 
-        Note: 
+        Note:
                 A value of 10 in dot_freq means that for every 10 spikes, only 1 will be plotted. This helps reducing lag when plotting heavy files.
         """
         if verbose:
             start_time = time.time()
-        #REPRESENTATION
+        # REPRESENTATION
         plt.style.use('seaborn-whitegrid')
         spk_fig = plt.figure()
         spk_fig.canvas.set_window_title(graph_title)
@@ -68,7 +65,8 @@ class Plots:
         mid_address = settings.num_channels * (settings.on_off_both + 1)
 
         if settings.mono_stereo == 0:
-            plt.scatter(spikes_file.timestamps[0::dot_freq], spikes_file.addresses[0::dot_freq], s=dot_size)
+            plt.scatter(spikes_file.timestamps[0::dot_freq], spikes_file.addresses[0::dot_freq],
+                        s=dot_size, rasterized=True)
         else:
             # Convert to numpy arrays
             addresses = np.array(spikes_file.addresses, copy=False)
@@ -86,9 +84,9 @@ class Plots:
             inf_timestamps = timestamps[-sup_indexes]
             inf_timestamps = inf_timestamps[::dot_freq]
 
-            plt.scatter(inf_timestamps, inf_addresses, s=dot_size, label="Left cochlea")
-            plt.scatter(sup_timestamps, sup_addresses, s=dot_size, label="Right cochlea")
-            plt.legend(fancybox=False, ncol=2, loc='upper center', markerscale=2/dot_size, frameon=True)
+            plt.scatter(inf_timestamps, inf_addresses, s=dot_size, label="Left cochlea", rasterized=True)
+            plt.scatter(sup_timestamps, sup_addresses, s=dot_size, label="Right cochlea", rasterized=True)
+            plt.legend(fancybox=False, ncol=2, loc='upper center', markerscale=2 / dot_size, frameon=True)
 
         if verbose:
             print('SPIKEGRAM CALCULATION', time.time() - start_time)
@@ -96,7 +94,7 @@ class Plots:
         plt.title(graph_title, fontsize='x-large')
         plt.xlabel('Timestamp ($\mu$s)', fontsize='large')
         plt.ylabel('Address', fontsize='large')
-        plt.ylim([0, mid_address*(settings.mono_stereo + 1)])
+        plt.ylim([0, mid_address * (settings.mono_stereo + 1)])
 
         if start_at_zero:
             max_timestamp = np.max(spikes_file.timestamps)
@@ -107,10 +105,10 @@ class Plots:
         return spk_fig
 
     @staticmethod
-    def sonogram(spikes_file, settings, return_data = False, graph_title = 'Sonogram', start_at_zero = True, verbose = False):
+    def sonogram(spikes_file, settings, return_data=False, graph_title='Sonogram', start_at_zero=True, verbose=False):
         """
         Plots the sonogram of a SpikesFile.
-        
+
         This is, a graph where the X axis means time and the Y axis represents addresses (or cochlea channels), and where the spiking activity is shown with color.
 
         Parameters:
@@ -167,7 +165,7 @@ class Plots:
             sng_fig = plt.figure()
             sng_fig.canvas.set_window_title(graph_title)
 
-            plt.imshow(sonogram, aspect="auto", cmap='hot')  # , aspect="auto")
+            plt.imshow(sonogram, aspect="auto", cmap='hot', rasterized=True)  # , aspect="auto")
             plt.gca().invert_yaxis()
 
             plt.xlabel('Bin (' + str(settings.bin_size) + '$\mu$s width)', fontsize='large')
@@ -204,10 +202,10 @@ class Plots:
             return sonogram
 
     @staticmethod
-    def histogram(spikes_file, settings, bar_line = 1, graph_title = 'Histogram', verbose = False):
+    def histogram(spikes_file, settings, bar_line=1, graph_title='Histogram', verbose=False):
         """
         Plots the histogram of a SpikesFile.
-        
+
         This is, a graph where addresses (or cochlea channels) are represented in the X axis, and number of spikes in the Y axis.
 
         Parameters:
@@ -223,7 +221,9 @@ class Plots:
 
         start_time = time.time()
 
-        spikes_count = np.bincount(spikes_file.addresses, minlength=settings.num_channels * (settings.on_off_both + 1) * (settings.mono_stereo + 1))
+        spikes_count = np.bincount(spikes_file.addresses,
+                                   minlength=settings.num_channels * (settings.on_off_both + 1) * (
+                                               settings.mono_stereo + 1))
 
         if verbose == True: print('HISTOGRAM CALCULATION:', time.time() - start_time)
 
@@ -236,17 +236,27 @@ class Plots:
 
         if bar_line == 0:
             if settings.mono_stereo == 1:
-                plt.bar(np.arange(settings.num_channels * (settings.on_off_both + 1)), spikes_count[0:settings.num_channels*(settings.on_off_both + 1)])
-                plt.bar(np.arange(settings.num_channels * (settings.on_off_both + 1)), spikes_count[settings.num_channels*(settings.on_off_both + 1):settings.num_channels*2*(settings.on_off_both + 1)])
+                plt.bar(np.arange(settings.num_channels * (settings.on_off_both + 1)),
+                        spikes_count[0:settings.num_channels * (settings.on_off_both + 1)],
+                        rasterized=True)
+                plt.bar(np.arange(settings.num_channels * (settings.on_off_both + 1)),
+                        spikes_count[settings.num_channels * (settings.on_off_both + 1):settings.num_channels * 2 * (settings.on_off_both + 1)],
+                        rasterized=True)
             else:
-                plt.bar(np.arange(settings.num_channels * (settings.on_off_both + 1) * (settings.mono_stereo + 1)), spikes_count)
+                plt.bar(np.arange(settings.num_channels * (settings.on_off_both + 1) * (settings.mono_stereo + 1)),
+                        spikes_count, rasterized=True)
         else:
             if settings.mono_stereo == 1:
-                plt.plot(np.arange(settings.num_channels * (settings.on_off_both + 1)), spikes_count[0:settings.num_channels*(settings.on_off_both + 1)], label='Left cochlea')
-                plt.plot(np.arange(settings.num_channels * (settings.on_off_both + 1)), spikes_count[settings.num_channels*(settings.on_off_both + 1):settings.num_channels*2*(settings.on_off_both + 1)], label='Right cochlea')
+                plt.plot(np.arange(settings.num_channels * (settings.on_off_both + 1)),
+                         spikes_count[0:settings.num_channels * (settings.on_off_both + 1)],
+                         label='Left cochlea', rasterized=True)
+                plt.plot(np.arange(settings.num_channels * (settings.on_off_both + 1)),
+                         spikes_count[settings.num_channels * (settings.on_off_both + 1):settings.num_channels * 2 * (settings.on_off_both + 1)],
+                         label='Right cochlea', rasterized=True)
                 plt.legend(loc='best', frameon=True)
             else:
-                plt.plot(np.arange(settings.num_channels * (settings.on_off_both + 1) * (settings.mono_stereo + 1)), spikes_count)
+                plt.plot(np.arange(settings.num_channels * (settings.on_off_both + 1) * (settings.mono_stereo + 1)),
+                         spikes_count, rasterized=True)
 
         plt.tight_layout()
 
@@ -315,7 +325,7 @@ class Plots:
         plt.xlabel('Bin (' + str(settings.bin_size) + '$\mu$s width)', fontsize='large')
         plt.ylabel('No. of spikes', fontsize='large')
 
-        plt.plot(bins / settings.bin_size, average_activity_L, label='Left cochlea')
+        plt.plot(bins / settings.bin_size, average_activity_L, label='Left cochlea', rasterized=True)
         if settings.mono_stereo == 1:
             plt.plot(bins / settings.bin_size, average_activity_R, label='Right cochlea')
             plt.legend(loc='best', ncol=2, frameon=True)
@@ -409,7 +419,7 @@ class Plots:
                 cmap_name = 'my_list'
                 cm = LinearSegmentedColormap.from_list(cmap_name, colors, N=100)
 
-                plt.imshow(diff, vmin=-100, vmax=100, aspect="auto", cmap=cm)  # , aspect="auto")
+                plt.imshow(diff, vmin=-100, vmax=100, aspect="auto", cmap=cm, rasterized=True)  # , aspect="auto")
                 plt.gca().invert_yaxis()
 
                 plt.xlabel('Bin (' + str(settings.bin_size) + '$\mu$s width)', fontsize='large')
@@ -427,11 +437,11 @@ class Plots:
                 return diff
         else:
             # print("This functionality is only available for stereo AEDAT files.")
-            print(
-                "[Plots.difference_between_LR] > SettingsError: This functionality is only available for stereo files.")
+            print("[Plots.difference_between_LR] > SettingsError: This functionality is only available for stereo files.")
 
     @staticmethod
-    def mso_heatmap(localization_file, localization_settings, graph_title = "MSO heatmap", enable_colorbar = True, verbose = False):
+    def mso_heatmap(localization_file, localization_settings, graph_title="MSO heatmap", enable_colorbar=True,
+                    verbose=False):
         """
         Plots the heatmap for the MSO activity extracted from a LocalizationFile.
 
@@ -443,7 +453,7 @@ class Plots:
                 graph_title (string, optional): Text that will appear as title for the graph.
                 enable_colorbar (boolean, optional): Set to True if you want to show the color bar that indicates the activity range by colors.
                 verbose (boolean, optional): Set to True if you want the execution time of the function to be printed.
-        
+
         Returns:
                 None.
 
@@ -461,7 +471,7 @@ class Plots:
 
         num_mso_events = len(localization_file.mso_timestamps)
 
-        for i in range (0, num_mso_events):
+        for i in range(0, num_mso_events):
             # Move the frequency channel from the relative range to an absolute range starting at zero
             freq_channel = localization_file.mso_channels[i] - localization_settings.mso_start_channel
             neuron_id = localization_file.mso_neuron_ids[i]
@@ -487,9 +497,9 @@ class Plots:
         # Create the heatmap image
         htmap_im = plt.imshow(mso_activity, cmap='viridis')
 
-        if enable_colorbar == True :
+        if enable_colorbar == True:
             colorbar = plt.colorbar()
-            colorbar.set_label('No. of spikes', rotation=270, fontsize='large', labelpad= 10)
+            colorbar.set_label('No. of spikes', rotation=270, fontsize='large', labelpad=10)
 
         # Set the ticks
         htmap_ax.set_xticks(np.arange(len(neuron_id_labels)))
@@ -503,13 +513,13 @@ class Plots:
 
         # Rotate the tick labels and set their alignment.
         plt.setp(htmap_ax.get_xticklabels(), rotation=45, ha="right",
-                rotation_mode="anchor")
+                 rotation_mode="anchor")
 
         # Loop over data dimensions and create text annotations.
         for i in range(len(freq_channel_labels)):
             for j in range(len(neuron_id_labels)):
                 text = htmap_ax.text(j, i, mso_activity[i, j],
-                            ha="center", va="center", color="w", fontsize='xx-small')
+                                     ha="center", va="center", color="w", fontsize='xx-small')
 
         plt.title(graph_title, fontsize='x-large')
 
@@ -517,7 +527,8 @@ class Plots:
         htmap_fig.show()
 
     @staticmethod
-    def mso_spikegram(localization_file, settings, localization_settings, dot_size = 0.2, graph_title = 'MSO spikegram', verbose = False):
+    def mso_spikegram(localization_file, settings, localization_settings, dot_size=0.2, graph_title='MSO spikegram',
+                      verbose=False):
         """
         Plots the 3D spikegram (also known as raster plot) of the MSO information contained in a LocalizationFile.
         This is, a graph where the X axis represents neuron IDs, the Y axis means time, and the Z axis represents the frequency channels of the cochlea, and where every spike is plotted as a dot.
@@ -533,7 +544,7 @@ class Plots:
         Returns:
                 None.
 
-        Note: 
+        Note:
                 None.
         """
 
@@ -546,7 +557,8 @@ class Plots:
         msospk_fig.canvas.set_window_title(graph_title)
 
         # Plot all the spikes stored in the localization_file
-        ax.scatter(localization_file.mso_neuron_ids, localization_file.mso_timestamps, localization_file.mso_channels, s=dot_size)
+        ax.scatter(localization_file.mso_neuron_ids, localization_file.mso_timestamps, localization_file.mso_channels,
+                   s=dot_size)
 
         if verbose == True: print('MSO SPIKEGRAM CALCULATION', time.time() - start_time)
 
@@ -566,7 +578,8 @@ class Plots:
         msospk_fig.show()
 
     @staticmethod
-    def mso_localization_plot(localization_file, settings, localization_settings, graph_title = "MSO localization estimation", start_at_zero = True, verbose = False):
+    def mso_localization_plot(localization_file, settings, localization_settings,
+                              graph_title="MSO localization estimation", start_at_zero=True, verbose=False):
         """
         Plots the result of the coincidence counters of the Jeffress model for the MSO according to the activity in the LocalizationFile.
 
@@ -579,7 +592,7 @@ class Plots:
                 graph_title (string, optional): Text that will appear as title for the graph.
                 start_at_zero (boolean, optional): If set to True, the X axis will start at 0, instead of starting at the minimum timestamp.
                 verbose (boolean, optional): Set to True if you want the execution time of the function to be printed.
-        
+
         Returns:
                 None.
 
@@ -600,14 +613,14 @@ class Plots:
             last_time = min(localization_file.mso_timestamps)
 
         # Estimate the number of time bins
-        its = int(math.ceil(total_time/settings.bin_size))
+        its = int(math.ceil(total_time / settings.bin_size))
 
         # Create the activity array
         mso_activity = np.zeros((its, localization_settings.mso_num_neurons_channel))
         mso_max_activity = np.zeros(its)
 
         # For each time bin, calculate the activity of all the MSO neurons and get the winner
-        for i in range (0, its):
+        for i in range(0, its):
             a = bisect_left(localization_file.mso_timestamps, last_time)
             b = bisect_right(localization_file.mso_timestamps, last_time + settings.bin_size)
 
@@ -630,7 +643,7 @@ class Plots:
         mso_loc_fig = plt.figure()
         mso_loc_fig.canvas.set_window_title(graph_title)
         plt.title(graph_title, fontsize='x-large')
-        plt.xlabel('Bin ('+str(settings.bin_size) + '$\mu$s width)', fontsize='large')
+        plt.xlabel('Bin (' + str(settings.bin_size) + '$\mu$s width)', fontsize='large')
         plt.ylabel('Position (in degrees)', fontsize='large')
         plt.ylim([-1, localization_settings.mso_num_neurons_channel + 1])
 
@@ -642,35 +655,35 @@ class Plots:
 
         plt.yticks(ticks=np.arange(localization_settings.mso_num_neurons_channel), labels=yticklabels, fontsize='small')
 
-        plt.plot(np.arange(math.ceil(total_time/settings.bin_size)), mso_max_activity, label='Position estimation')
+        plt.plot(np.arange(math.ceil(total_time / settings.bin_size)), mso_max_activity, label='Position estimation')
 
         # Add some text to clarify the results
         plt.annotate('Left side',
-        xy=(1.00, 0.85), xytext=(5, 0),
-        xycoords=('axes fraction', 'axes fraction'),
-        textcoords='offset points',
-        size=11, ha='center', va='center', rotation=270)
+                     xy=(1.00, 0.85), xytext=(5, 0),
+                     xycoords=('axes fraction', 'axes fraction'),
+                     textcoords='offset points',
+                     size=11, ha='center', va='center', rotation=270)
 
         plt.annotate('Centre',
-        xy=(1.00, 0.50), xytext=(5, 0),
-        xycoords=('axes fraction', 'axes fraction'),
-        textcoords='offset points',
-        size=11, ha='center', va='center', rotation=270)
+                     xy=(1.00, 0.50), xytext=(5, 0),
+                     xycoords=('axes fraction', 'axes fraction'),
+                     textcoords='offset points',
+                     size=11, ha='center', va='center', rotation=270)
 
         plt.annotate('Right side',
-        xy=(1.00, 0.15), xytext=(5, 0),
-        xycoords=('axes fraction', 'axes fraction'),
-        textcoords='offset points',
-        size=11, ha='center', va='center', rotation=270)
+                     xy=(1.00, 0.15), xytext=(5, 0),
+                     xycoords=('axes fraction', 'axes fraction'),
+                     textcoords='offset points',
+                     size=11, ha='center', va='center', rotation=270)
 
         plt.tight_layout()
         mso_loc_fig.show()
 
     @staticmethod
-    def mso_histogram(localization_file, settings, localization_settings, graph_title = 'MSO histogram', verbose = False):
+    def mso_histogram(localization_file, settings, localization_settings, graph_title='MSO histogram', verbose=False):
         """
         Plots the 3D histogram of the MSO information contained in a LocalizationFile.
-        
+
         This is, a graph where neuron IDs are represented in the X axis, frequency channels are represented in the Y axis, and number of spikes in the Z axis.
 
         Parameters:
@@ -682,7 +695,7 @@ class Plots:
 
         Returns:
                 None.
-        
+
         Note:
                 None.
         """
@@ -698,7 +711,7 @@ class Plots:
         num_mso_events = len(localization_file.mso_timestamps)
 
         # Calculate the total number of events for each frequency channel and for each neuron ID
-        for i in range (0, num_mso_events):
+        for i in range(0, num_mso_events):
             freq_channel = localization_file.mso_channels[i] - localization_settings.mso_start_channel
             neuron_id = localization_file.mso_neuron_ids[i]
             # Accumulate the activity for each neuron for each frequency channel according to the LocalizationFIle
@@ -726,9 +739,9 @@ class Plots:
         ax.set_title('Shaded')
 
         # Set the axes' limits
-        ax.set_xlim3d(0,localization_settings.mso_num_neurons_channel)
-        ax.set_ylim3d(0,mso_number_freq_ch)
-        ax.set_zlim3d(0,np.amax(z))
+        ax.set_xlim3d(0, localization_settings.mso_num_neurons_channel)
+        ax.set_ylim3d(0, mso_number_freq_ch)
+        ax.set_zlim3d(0, np.amax(z))
 
         # Set the axes' labels
         ax.set_xlabel('Neuron ID', fontsize='large')
--- src/pyNAVIS/report_functions.py
+++ src/pyNAVIS/report_functions.py
@@ -1,12 +1,14 @@
-from .loaders import Loaders
+import datetime
+import os
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
 from .functions import Functions
+from .loaders import Loaders
 from .objects import LocalizationFile
 from .objects import SpikesFile
 from .plots import Plots
-import matplotlib.pyplot as plt
-import matplotlib.backends.backend_pdf
-import datetime
-import os
 
 
 class ReportFunctions:
@@ -18,26 +20,26 @@ class ReportFunctions:
                    localization_plots=["MSO spikegram", "MSO heatmap", "MSO histogram", "MSO localization"],
                    vector=False, verbose=False):
         """
-        Generates a PDF report with the spikegram, sonogram, histogram, average activity and difference between L/R plots obtained from the input SpikesFile or path containing SpikeFiles.
+		Generates a PDF report with the spikegram, sonogram, histogram, average activity and difference between L/R plots obtained from the input SpikesFile or path containing SpikeFiles.
 
-        Parameters:
-                spikes_file (SpikesFile or string): File or path to use.
-                settings (MainSettings): Configuration parameters for the input file.
-                output_path (string): Destination path.
-                plots (string[]): List to select the plots to be included in the PDF report.
-                add_localization_report (boolean, optional): If True, the localization plots will be included in the PDF report.
-                localization_file (LocalizationFile, optional): If add_localization_report is set to True, this parameter is mandatory, and it should contain the localization information.
-                localization_settings (LocalizationSettings, optional): If add_localization_report is set to True, this parameter is mandatory, and it should contain the localization settings.
-                localization_plots (string[], optional): If add_localization_report is set to True, this parameter is mandatory, and it should contain the list of localization plots.
-                vector (boolean, optional): Set to True if you want the Spikegram plot vectorized. Note: this may make your PDF heavy.
-                verbose (boolean, optional): Set to True if you want the execution time of the function to be printed.
+		Parameters:
+				spikes_file (SpikesFile or string): File or path to use.
+				settings (MainSettings): Configuration parameters for the input file.
+				output_path (string): Destination path.
+				plots (string[]): List to select the plots to be included in the PDF report.
+				add_localization_report (boolean, optional): If True, the localization plots will be included in the PDF report.
+				localization_file (LocalizationFile, optional): If add_localization_report is set to True, this parameter is mandatory, and it should contain the localization information.
+				localization_settings (LocalizationSettings, optional): If add_localization_report is set to True, this parameter is mandatory, and it should contain the localization settings.
+				localization_plots (string[], optional): If add_localization_report is set to True, this parameter is mandatory, and it should contain the list of localization plots.
+				vector (boolean, optional): Set to True if you want the Spikegram plot vectorized. Note: this may make your PDF heavy.
+				verbose (boolean, optional): Set to True if you want the execution time of the function to be printed.
 
-        Returns:
-                None.
+		Returns:
+				None.
 
-        Notes:
-                If the path used as input is a folder instead of a spikes file, the PDF report is generated for every spikes file contained in the folder.
-        """
+		Notes:
+				If the path used as input is a folder instead of a spikes file, the PDF report is generated for every spikes file contained in the folder.
+		"""
 
         if isinstance(spikes_file, str):
 
@@ -71,34 +73,36 @@ class ReportFunctions:
                 localization_file.timestamps = Functions.adapt_timestamps(localization_file.timestamps, settings)
 
         if isinstance(spikes_file, SpikesFile):
-
-            pdf = matplotlib.backends.backend_pdf.PdfPages(output_path)
+            pdf = PdfPages(output_path)
+            figures = list()
 
             # Spikegram
-            if any("Spikegram" in s for s in plots):
-                spikegram = Plots.spikegram(spikes_file, settings, )
-                pdf.savefig(spikegram)
-                plt.draw()
+            if "Spikegram" in plots:
+                spk_fig = Plots.spikegram(spikes_file, settings)
+                figures.append(spk_fig)
+
             # Sonogram
-            if any("Sonogram" in s for s in plots):
-                sonogram = Plots.sonogram(spikes_file, settings)
-                pdf.savefig(sonogram)
-                plt.draw()
+            if "Sonogram" in plots:
+                sng_fig = Plots.sonogram(spikes_file, settings)
+                figures.append(sng_fig)
+
             # Histogram
-            if any("Histogram" in s for s in plots):
-                Plots.histogram(spikes_file, settings, )
-                pdf.savefig()
-                plt.draw()
+            if "Histogram" in plots:
+                _, hst_fig = Plots.histogram(spikes_file, settings)
+                figures.append(hst_fig)
+
             # Average activity
-            if any("Average activity" in s for s in plots):
-                Plots.average_activity(spikes_file, settings, )
-                pdf.savefig()
-                plt.draw()
+            if "Average activity" in plots:
+                if settings.mono_stereo == 0:
+                    _, avg_fig = Plots.average_activity(spikes_file, settings)
+                else:
+                    _, _, avg_fig = Plots.average_activity(spikes_file, settings)
+                figures.append(avg_fig)
+
             # Difference between L/R
-            if settings.mono_stereo == 1 and any("Difference between L/R" in s for s in plots):
-                Plots.difference_between_LR(spikes_file, settings, )
-                pdf.savefig()
-                plt.draw()
+            if "Difference between L/R" in plots:
+                dlr_fig = Plots.difference_between_LR(spikes_file, settings)
+                figures.append(dlr_fig)
 
             if add_localization_report == True:
                 if isinstance(localization_file, LocalizationFile):
@@ -126,6 +130,9 @@ class ReportFunctions:
                 else:
                     print("[Functions.PDF_report] > InputFileError: the input LocalizationFile is not valid.")
 
+            for fig in figures:
+                pdf.savefig(fig, dpi=300)
+
             d = pdf.infodict()
             d['Title'] = 'pyNAVIS report'
             d['Author'] = 'Juan P. Dominguez-Morales'
@@ -135,7 +142,6 @@ class ReportFunctions:
             d['ModDate'] = datetime.datetime.today()
 
             pdf.close()
-            plt.close()
             print("[Functions.PDF_report] > PDF report generated correctly")
 
         else:
