550def294b4ff08bb4ef806b1db712318b49aa5f
Juan Pedro Dominguez-Morales <jpdominguez@atc.us.es>
2023-05-24 11:52:45 -0700

    updated version

--- src/setup.py
+++ src/setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 """
 setup_args = dict(
     name='pyNAVIS',
-    version='1.1.4',
+    version='1.1.6',
     description='Useful tools to analyze and process spiking information from neuromorphic auditory sensors.',
     long_description_content_type="text/markdown",
     long_description=README,
