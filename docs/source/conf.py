2ba7dda033ba1aab07f983c6752e4bf4f098daec
jpdominguez <juadommor2@gmail.com>
2023-11-15 16:36:34 +0100

    fixed datagenerators

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2018, Juan P. Dominguez-Morales'
 author = 'Juan P. Dominguez-Morales'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.7'
+release = '1.1.8'
 
 
 # -- General configuration ---------------------------------------------------
