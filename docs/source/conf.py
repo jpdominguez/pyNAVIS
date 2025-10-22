cbc30c9ca03b9069dcd435ae43f1ffce2978d03f
jpdominguez <juadommor2@gmail.com>
2025-10-22 17:47:26 +0200

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2018, Juan P. Dominguez-Morales'
 author = 'Juan P. Dominguez-Morales'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.4'
+release = '1.2.5'
 
 
 # -- General configuration ---------------------------------------------------
